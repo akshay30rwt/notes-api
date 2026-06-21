87ab08284917b5261c587471639aa8d286d9a6f8
akshay30rwt <akshayrwt8700@gmail.com>
2026-06-21 18:52:51 +0530

    Add GET and POST routes for notes

--- index.js
+++ index.js
@@ -3,8 +3,32 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+app.use(express.json());
+
+let notes = [];
+let nextId = 1;
+
 app.get('/', (req, res) => {
     res.send('Notes API is running');
+});
+
+app.get('/notes', (req, res) => {
+    res.json(notes);
+});
+
+app.post('/notes', (req, res) => {
+    const { title, content } = req.body;
+
+    const newNote = {
+        id: nextId,
+        title: title,
+        content: content
+    }
+
+    nextId++;
+    notes.push(newNote);
+
+    res.status(201).json(newNote);
 });
 
 app.listen(PORT, () => {
