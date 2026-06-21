700e729a98e1e9624020f3da6d6fde45714da5c2
akshay30rwt <akshayrwt8700@gmail.com>
2026-06-21 19:58:48 +0530

    Add DELETE and PUT routes for notes

--- index.js
+++ index.js
@@ -31,6 +31,34 @@ app.post('/notes', (req, res) => {
     res.status(201).json(newNote);
 });
 
+app.delete('/notes/:id', (req, res) => {
+    const noteId = Number(req.params.id);
+    const index = notes.findIndex(note => note.id === noteId);
+
+    if(index === -1) {
+        res.status(404).json({ message: `Note with ID ${noteId} not found` });
+        return;
+    }
+
+    const deletedNote = notes[index].title;
+    notes.splice(index, 1);
+    res.status(200).json({ message: `Note with title: '${deletedNote}' deleted.` })
+});
+
+app.put('/notes/:id', (req, res) => {
+    const { title, content } = req.body;
+    const noteId = Number(req.params.id);
+    const index = notes.findIndex(note => note.id === noteId);
+
+    if(index === -1) {
+        res.status(404).json({ message: 'Note not found' });
+        return;
+    }
+    notes[index].title = title;
+    notes[index].content = content
+    res.status(200).json({ message: `Note ${noteId} updated, title: '${notes[index].title}'` });
+});
+
 app.listen(PORT, () => {
     console.log(`Server is running on port: ${PORT}`);
 });
